84e1cca2ca595df987d5ca341153334d99c95504
Sam Terlop <samterlop@Sams-MBP.home>
2023-06-02 19:10:50 -0400

    first express app

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
-
+require('dotenv').config();
 
 http.createServer().listen(process.env.PORT, () => {
-    console.log('Node is up and running');
+    console.log('Node is up and running on port ${process.env.PORT}');
 });
