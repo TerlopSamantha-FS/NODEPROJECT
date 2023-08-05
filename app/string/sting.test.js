124caac6ee5d4d35da842208d6ea3cf3e3f85631
Sam Terlop <samterlop@Sams-MBP.home>
2023-08-05 17:39:21 -0400

    New test for the string module

---
+++ app/string/sting.test.js
@@ -0,0 +1,17 @@
+const {
+    uppercase,
+    lowercase
+} = require("./string")
+
+describe("Testing the string module", () => {
+    
+    test("should uppercase a string input", () => {
+      
+        expect(uppercase("bob")).toBe("BOB");
+    });
+
+    test("should lowercase a string input", () => {
+        expect(lowercase("FULLSaiL")).toBe("fullsail");
+    });
+
+});
